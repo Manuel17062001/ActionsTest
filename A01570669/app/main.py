f655eebd1cfc6e92cfc81e94ff31e2e2d33e430b
Manuel Tec <72881652+Manuel17062001@users.noreply.github.com>
2023-06-11 20:52:26 -0600

    Update main.py

--- A01570669/app/main.py
+++ A01570669/app/main.py
@@ -1,8 +1,14 @@
 """
-Hello World
+This module contains a simple function to print "Hello, World!".
 """
 
 def hello_world():
+    """
+    Prints and returns the string "Hello, World!".
+
+    Returns:
+        str: The string "Hello, World!".
+    """
     return "Hello, World!"
 
 print(hello_world())
